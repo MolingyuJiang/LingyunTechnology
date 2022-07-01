f8c4abec8e6484965a603a8b95596db53b644b1b
MolingyuJiang <MolingyuJiang@163.com>
2022-07-01 23:57:25 +0800

    优化登录权限,生成token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,8 +1,12 @@
 // 用户相关的控制器;
+//导入jsonwebtoken(用于生成token);
+const jwt = require('jsonwebtoken');
+// 导入常量(.env文件);
+const { JWT_SECRET } = require('../config/config.default')
 // 导入数据库相关操作;
-const { createUser } = require('../service/user.service');
+const { createUser, getUserInfo } = require('../service/user.service');
 // 导入错误类型;
-const { userRegisterError } = require('../constant/user.err.type')
+const { userRegisterError, userLoginError } = require('../constant/user.err.type');
 class UserController {
   // 用户注册;
   async userRegister(ctx, next) {
@@ -28,8 +32,23 @@ class UserController {
   async userLogin(ctx, next) {
     //获取用户请求信息中的数据;
     const { user_name, user_password } = ctx.request.body;
-    ctx.body = user_name;
-  }
+    try {
+      //根据用户名进行数据库查询并剔除不需要的属性;
+      const { user_password, ...res } = await getUserInfo({ user_name });
+      //返回信息;
+      ctx.body = {
+        code: 0,
+        message: '用户登录成功',
+        result: {
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: '1d' })
+        }
+      };
+    } catch (error) {
+      console.error('用户登陆失败', error);
+      ctx.app.emit('error', userLoginError, ctx);
+      return;
+    };
+  };
 
 
 };
